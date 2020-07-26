3456de93e0b017b14e5d53e8cd6528e50b1da24c
yrjkqq <yuangu.1994@gmail.com>
2020-07-26 17:50:17 +0800

    graceful shutdown

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/astaxie/beego v1.12.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/go-redis/redis/v8 v8.0.0-beta.5
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/yrjkqq/tiny-website/pkg/gredis"
 	"github.com/yrjkqq/tiny-website/pkg/setting"
@@ -29,5 +34,39 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
+
+	// endless 在 windows 下面无法使用, 有几个信号 windows 下未定义
+	// endless.DefaultReadTimeOut = setting.ReadTimeout
+	// endless.DefaultWriteTimeOut = setting.WriteTimeout
+	// endless.DefaultMaxHeaderBytes = 1 << 20
+	// endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+
+	// server := endless.NewServer(endPoint, routers.InitRouter())
+	// server.BeforeBegin = func(add string) {
+	// 	log.Printf("Actual pid is %d", syscall.Getpid())
+	// }
+
+	// err := server.ListenAndServe()
+	// if err != nil {
+	// 	log.Printf("Serve err: %v", err)
+	// }
 }
